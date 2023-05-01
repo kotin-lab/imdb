51517b5f734031715e69033d85902fb602bee13c
Kotin Lab <tinhtunaungcu.cu@gmail.com>
2023-05-01 18:14:06 +0630

    Fetch data from TMDB api in the Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,30 @@
-import Image from 'next/image'
+import Results from "@/components/Results";
 
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({searchParams}) {
+  let genre = searchParams.genre || 'fetchTranding';
+  genre = genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/all/week';
+  const apiUrl = `https://api.themoviedb.org/3/${genre}?api_key=${API_KEY}&language=en-US&page=1`;
+  
+  const res = await fetch(apiUrl, {
+    next: {
+      revalidate: 10000
+    }
+  });
+
+  if (!res.ok) {
+    // This will be caught by the error page 
+    // and passed to the page as props
+    throw new Error('Failed to fetch data');
+  }
+
+  const data = await res.json();
+  const results = data.results;
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>HOME</h1>
+      <Results results={results} />
     </main>
   )
 }
